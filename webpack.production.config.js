0ade45f5580bd301c6fcc42ae128cedca96c7b64
jerrymao15 <jerryy.mao@gmail.com>
2016-10-24 15:54:55 -0700

    hella boilerplate

---
+++ webpack.production.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+
+module.exports = {
+  entry: './src/index',
+  output: {
+    path: path.join(__dirname, '/src/bundle/'),
+    filename: 'app.js',
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader:  'babel-loader',
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'),
+    },
+    {
+      test: /\.sass$|\.scss$/,
+      loader: ExtractTextPlugin.extract('style', 'css!sass!postcss?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'),
+    },
+    ],
+  },
+  postcss: [
+    require('autoprefixer-core'),
+  ],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
+    }),
+    new ExtractTextPlugin('style.css', { allChunks: true }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+    }),
+  ],
+};
---
+++ webpack.test.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  output: {
+    // YOU NEED TO SET libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.sass$|\.scss$/,
+        loaders: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]',
+          'postcss-loader',
+          'sass-loader?sourceMap',
+        ],
+      },
+    ],
+  },
+};
